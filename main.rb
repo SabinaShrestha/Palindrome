733ce4f8fb0bcddeb76335b4b5c388a028120a05
Sabina Shrestha <sabina.pari@yahoo.com>
2018-05-18 10:25:45 -0600

    fix output

--- main.rb
+++ main.rb
@@ -2,29 +2,27 @@ require "pry"
 require "colorize"
 
 @get_arr = []
+@arr = []
 @get_input = []
 
 
 def check
   puts "Please enter a word to check palindrome."
   @get_input << gets.strip.downcase
-  # binding.pry
   @arr = @get_input.first.split("")
+
   i = @arr.length
-  # @get_arr = []
   while i > 0
     @get_arr << @arr[i - 1]
     i -= 1
   end
-# binding.pry
+
   if @get_arr == @arr
     puts "You got palindrome."
   else
     puts "NO."
   end
-
-
-   puts @get_arr
+   # puts @get_arr
 end
 
 check
